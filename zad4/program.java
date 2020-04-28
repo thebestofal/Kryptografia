8462262588bcdaecb87a7ab2c19987126d383d2d
Marek Skrzypkowski <marek.skrzypkowski98@gmail.com>
2020-04-28 21:51:28 +0200

    zad4 added

---
+++ zad4/program.java
@@ -0,0 +1,24 @@
+class Main
+{
+	public static void main(String[] args)
+	{
+		String personal = args[0];
+		String personal_ = args[1];
+		
+		byte[] b_personal = personal.getBytes();
+		byte[] b_personal_ = personal_.getBytes();
+		int same_bits = 0;
+		for(int i = 0; i < b_personal.length; i++)
+		{
+			long a = Character.getNumericValue(args[0].charAt(i));
+			long b = Character.getNumericValue(args[1].charAt(i));
+			same_bits += Long.bitCount( a ^ b);
+		}
+		
+		System.out.println("******* "+same_bits+" *********");
+		int diff_bits = b_personal.length*4 - same_bits;
+		System.out.print("Liczba rozniacych sie bitow: " + diff_bits
+		+" z "+b_personal.length*4 + ", procentowo: " +
+		((double)diff_bits / (b_personal.length*4))*100 + "%.");
+	}
+}
